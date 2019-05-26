425a89b3da8f34810b5f22646b376dae2930a79c
Brian Berry <brian@bwantech.net>
2019-05-26 07:27:15 -0400

    More updates

--- .custom_funcs.sh
+++ .custom_funcs.sh
@@ -34,21 +34,71 @@ dir_size()
 }
 
 mygrep () {
-    find . -name "$2"  2>/dev/null -exec grep "$1" {} +
+    if [ "$#" -ne 2 ]; then
+        echo "USAGE : mygrep srchstr filespec"
+        echo "Hint: Wildcards must be quoted"
+    else
+        find . -name "$2"  2>/dev/null -exec grep "$1" {} +
+    fi
 }
 
 ffind() {
-    find . -name "$1*" 2>/dev/null
+    if [ "$#" -ne 1 ]; then
+        echo "USAGE : ffind filename"
+    else
+        find . -name "$1*" 2>/dev/null
+    fi
 }
 
 fext() {
-    find . -type f -name "*.$1" 2>/dev/null
+    if [ "$#" -ne 1 ]; then
+        echo "USAGE : fext extension"
+    else
+        find . -type f -maxdepth 1 -name "*.$1" 2>/dev/null
+    fi
+}
+
+fext2() {
+    if [ "$#" -ne 1 ]; then
+        echo "USAGE : fext2 extension"
+    else
+        find . -type f -name "*.$1" 2>/dev/null
+    fi
 }
 
 show_funcs() {
     cat ~/dotfiles/.custom_funcs.sh | less
 }
 
+reload() {
+    if [ -n "$ZSH_VERSION" ]; then
+        if [ -f ~/.zshrc ]; then
+            source ~/.zshrc
+        fi
+    elif [ -n "$BASH_VERSION" ]; then
+        if [ -f ~/.bashrc ]; then
+            source ~/.bashrc
+        fi
+    else
+       # asume something else
+    fi
+}
+
+mcd () {
+    case $# in
+    1)
+        [ -d $1 ] || mkdir -p $1
+        cd $1 && pwd
+        ;;
+    *)
+        echo "USAGE : mcd rep"
+        ;;
+    esac
+}
+
+finda() {
+    alias | grep "$1"
+}
 #####################################
 # End custom functions
 #####################################
